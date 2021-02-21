4f7a1a42247058d7503e86904aa6a25b20b49188
Mr. Developer <77911154+MrBotDeveloper@users.noreply.github.com>
2021-02-21 16:04:31 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -17,7 +17,7 @@ You can convert any Telegram Document, using this bot! With custom thumbnail
     DEL_ETED_CUSTOM_THUMB_NAIL = "✅ Custom thumbnail cleared succesfully."
     FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "✅ Media cleared succesfully."
     SAVED_RECVD_DOC_FILE = "Document Downloaded Successfully."
-    CUSTOM_CAPTION_UL_FILE = ""
+    CUSTOM_CAPTION_UL_FILE = "<b> By https://t.me/File2Video_robot</b>"
     NO_CUSTOM_THUMB_NAIL_FOUND = "No Custom ThumbNail found."
     CURENT_PLAN_DETAILS = """Current plan details
 --------
